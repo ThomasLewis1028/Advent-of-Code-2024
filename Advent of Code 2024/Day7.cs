834ac70ca7cf120dbd316768bac5328b6d57323c
Thomas Lewis <thomas.a.lewis@proton.me>
2024-12-07 17:50:06 -0700

    Finished Day 7

--- Advent of Code 2024/Day7.cs
+++ Advent of Code 2024/Day7.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Advent_of_Code_2024;
 
 public static class Day7
@@ -6,28 +8,118 @@ public static class Day7
     {
         Console.WriteLine("--- Day 7: Bridge Repair ---");
 
-        List<List<char>> map = File
+        List<string> input = File
             .ReadAllText("input/day7.txt")
             .Trim()
             .Split("\n")
-            .Select(line => line
-                .ToCharArray()
-                .ToList())
             .ToList();
 
+        List<(long answer, List<long> values)> equations = new List<(long, List<long>)>();
 
-        Console.WriteLine("\tPart 1: " + Part1());
-        Console.WriteLine("\tPart 2: " + Part2());
+        foreach (var line in input)
+        {
+            var answer = long
+                .Parse(line
+                    .Split(":")[0]);
+
+            var values = line
+                .Split(": ")[1]
+                .Split(" ")
+                .Select(long
+                    .Parse)
+                .ToList();
+
+            equations.Add((answer, values));
+        }
+
+
+        Console.WriteLine("\tPart 1: " + Part1(equations));
+        Console.WriteLine("\tPart 2: " + Part2(equations));
     }
 
-    private static int Part1()
+    private static long Part1(List<(long answer, List<long> values)> equations)
     {
-        
-        return 0;
+        long sum = 0;
+
+        foreach (var equation in equations)
+        {
+            List<long> eqAnswers = new List<long>();
+
+            Calculate(eqAnswers, equation.values, "", 0);
+
+            if (eqAnswers.Contains(equation.answer))
+                sum += equation.answer;
+        }
+
+        return sum;
     }
 
-    private static int Part2()
+    private static long Part2(List<(long answer, List<long> values)> equations)
     {
-        return 0;
+        long sum = 0;
+
+        foreach (var equation in equations)
+        {
+            List<long> eqAnswers = new List<long>();
+
+            Calculate(eqAnswers, equation.values, "", 0, true);
+
+            if (eqAnswers.Contains(equation.answer))
+                sum += equation.answer;
+        }
+
+        return sum;
+    }
+
+    private static List<long> Calculate(List<long> eqAnswers, List<long> values, string equation, int index,
+        bool pt2 = false)
+    {
+        if (index <= values.Count - 1)
+        {
+            for (int i = index; i < values.Count; i++)
+            {
+                if (i != values.Count - 1)
+                {
+                    Calculate(eqAnswers, values, equation + values[i] + " + ", i + 1, pt2);
+                    Calculate(eqAnswers, values, equation + values[i] + " * ", i + 1, pt2);
+                    if (pt2)
+                        Calculate(eqAnswers, values, equation + values[i] + " || ", i + 1, pt2);
+                }
+                else
+                {
+                    equation += values[i];
+                }
+            }
+        }
+
+        if (index == values.Count - 1)
+        {
+            var eq = equation.Split(' ');
+
+            var answer = long.Parse(eq[0]);
+
+            for (var i = 1; i < eq.Length; i++)
+            {
+                switch (eq[i])
+                {
+                    case "+":
+                        answer += long.Parse(eq[i + 1]);
+                        i++;
+                        break;
+                    case "*":
+                        answer *= long.Parse(eq[i + 1]);
+                        i++;
+                        break;
+                    case "||":
+                        answer = Convert.ToInt64("" + answer + "" + long.Parse(eq[i + 1]));
+                        i++;
+                        break;
+                }
+            }
+
+            eqAnswers.Add(answer);
+        }
+
+        return eqAnswers;
     }
 }
